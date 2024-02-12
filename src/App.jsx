14504501c12b70d8950169ba0a95d8f21468bca4
TaylorDev <jhontmg26@gmail.com>
2024-02-11 22:32:37 -0300

    " añadiendo imagenes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect, useRef} from "react";
 import "./App.scss";
+import { images } from "./images";
 useState;
 function App() {
   return (
     <>
       <div>
-        <h1>Efecto de seguimiento del mouse con perspectiva 3D</h1>
 
         <Container></Container>
         
-        <MouseTracker></MouseTracker>
+
       </div>
     </>
   );
@@ -17,25 +17,25 @@ function App() {
 
 export default App;
 
-
 const Container = () => {
   const [rotation, setRotation] = useState({ x: 0, y: 0 });
+  const containerRef = useRef(null);
 
   useEffect(() => {
     const handleMouseMove = (e) => {
-      const container = document.getElementById("container");
+      const container = containerRef.current;
+      if (!container) return;
+      
       const { left, top, width, height } = container.getBoundingClientRect();
       const centerX = left + width / 2;
       const centerY = top + height / 2;
-      const angleXRad = Math.atan2(e.clientY - centerY, 300);
-      const angleYRad = Math.atan2(e.clientX - centerX, 300);
-      const angleXDeg = angleXRad * (180 / Math.PI);
-      const angleYDeg = angleYRad * (180 / Math.PI);
+      
+      const angleXDeg = Math.atan2(e.clientY - centerY, 300) * (180 / Math.PI);
+      const angleYDeg = Math.atan2(e.clientX - centerX, 300) * (180 / Math.PI);
+      
       setRotation({ x: angleXDeg, y: angleYDeg });
     };
-
     window.addEventListener("mousemove", handleMouseMove);
-
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
@@ -43,156 +43,119 @@ const Container = () => {
 
   //var a = -30;
   var a = -rotation.x; //-30
-  var b = rotation.y; //+30
-  var c = 0;
-  var d = -b;
-  var e = -a;
+  var b =  rotation.y; //+30
+  const cubo = {
+
+    transform: `rotateX(${a}deg) rotateY(${b}deg) `,
+    
+  };
+
+  
+
+
+
+
+
+
+
+  
+
+
+
+
 
   return (
-    <div id="container">
-      <div className="container">
-        <RotatingDiv id="1" rotateX={a} rotateY={b} numero={1}></RotatingDiv>
-        <RotatingDiv id="2" rotateX={a} rotateY={b} numero={2}></RotatingDiv>
-        <RotatingDiv id="3" rotateX={a} rotateY={b}></RotatingDiv>
+    <div id="canvas">
+      <div ref={containerRef} className="container">
+   
+        {images.map(image =>(
+
+            
+            <Objeto3d key={image.id } rotateX={a} rotateY={b}  anchor={{x:50, y:50}}img={image.imageUrl} cover={image.coverUrl}></Objeto3d>
+
+        ))}
       </div>
 
+      <button style={{backgroundColor: `${false? "green": "red"}`}}   onClick={toggleScrollTracking} >
+        {false ? 'Desactivar Seguimiento de Scroll' : 'Activar Seguimiento de Scroll'}
+      </button>
+ 
+    </div>
+  );
+};
 
-     
+
+
+export const Objeto3d = ({ rotateX, rotateY,  anchor={x:50, y:50}, img, cover, }) => {
+
+
+
+
+  const divStyle = {
+    transformOrigin: `${anchor.x}% ${anchor.y}%`,
+    transform: `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
+    
+  };
+  
+
+
+  return (
+    
+      <div className="objecto3d" style={divStyle}  > 
        
+       <div className="centro">
+        <div
+            className="anchor"
+            style={{ left: `${anchor.x}%`, top: `${anchor.y}%` }}
+          ></div>
+          <div
+            className="anchor2"
+            style={{ left: `${anchor.x}%`, top: `${anchor.y}%` }}
+          ></div>
+          <div
+            className="anchor3"
+            style={{ left: `${anchor.x}%`, top: `${anchor.y}%` }}
+          ></div>
+       </div>
+    
+        
+        <div className="face front"  >
+          <img className="marco" src={cover} alt="" />
+        </div>
+       
+        <div className="face back" >
+
+        <img className="foto" src={img} alt="" />
+        </div>
 
       
-   
-    </div>
-  );
-};
-
-export const RotatingDiv = ({ rotateX, rotateY, numero }) => {
-  const [anchor, setAnchor] = useState({ x: 0, y: 0 })
-  const divStyle = {
-    transformOrigin: `${anchor.x}px ${ anchor.y}px`,
-    transform: `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
-  };
-
-
-  const handleAnchor =(x, y)=>{
-    setAnchor({ x, y });
-  }
-  const seleccionarImagen = (numero) => {
-    if (numero === 1) {
-      return (
-        <img
-          src="https://i.pinimg.com/736x/c7/02/0f/c7020f6a8f96a6172fe197db9ed674b4.jpg"
-          alt="Imagen 1"
-        />
-      );
-    } else if (numero === 2) {
-      return (
-        <img
-          src="https://i.pinimg.com/564x/bf/20/bd/bf20bd054e5cbf581e05230ec9d969f8.jpg"
-          alt="Imagen 2"
-        />
-      );
-    } else {
-      return (
-        <img
-          src="https://i.pinimg.com/564x/47/f3/46/47f346e3e9bf57c301950bdbebb8d2ca.jpg"
-          alt="Imagen por defecto"
-        />
-      );
-    }
-  };
-
-  return (
-    <div className="rotating-div" style={divStyle}>
-      {/*seleccionarImagen(numero)*/}
-    
-      <ContainerWithAnchor setAnchoPos={handleAnchor}></ContainerWithAnchor>
-    </div>
-  );
-};
-
-
-const ContainerWithAnchor = ({setAnchoPos}) => {
-  const parentRef = useRef(null);
-  const childRef = useRef(null);
-  const [localPosition, setLocalPosition] = useState({ x: 0, y: 0 });
-  const [globalPosition, setGlobalPosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const updatePosition = () => {
-      if (childRef.current && parentRef.current) {
-        const parentRect = parentRef.current.getBoundingClientRect();
-        const childRect = childRef.current.getBoundingClientRect();
-        
-        const localX = childRect.left - parentRect.left;
-        const localY = childRect.top - parentRect.top;
-        
-        const globalX = childRect.left + window.scrollX;
-        const globalY = childRect.top + window.scrollY;
-
-        setLocalPosition({ x: localX, y: localY });
-        setGlobalPosition({ x: globalX, y: globalY });
-       
-      }
-    };
-   
-    updatePosition();
-    window.addEventListener('mousemove', updatePosition);
-    return () => {
-      window.removeEventListener('mousemove', updatePosition);
-    };
-  }, []);
-
-
-  useEffect(()=>{
-
-    setAnchoPos(localPosition.x, localPosition.y);
-  },[localPosition,globalPosition])
-
-  return (
-    <div>
-      <div
-        ref={parentRef}
-        style={{ width: '200px', height: '200px', border: '1px solid white', position: 'relative' }}
-      >
-        <div
-          ref={childRef}
-          style={{ width: '20px', height: '20px', background: 'red', position: 'absolute', top: '0px', left: '100px' }}
-        ></div>
-      </div>
-
-      <div>
-        Local  <br />
-        X: {localPosition.x},  
-        Y: {localPosition.y}
-      </div>
-      <div>
-        Global <br />
-        X:{globalPosition.x},
-          Y: {globalPosition.y}
       </div>
     
-    </div>
   );
 };
-const MouseTracker = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const updateMousePosition = (e) => {
-      setPosition({ x: e.clientX, y: e.clientY });
-    };
 
-    window.addEventListener('mousemove', updateMousePosition);
 
-    return () => {
-      window.removeEventListener('mousemove', updateMousePosition);
-    };
-  }, []); // Solo se suscribe una vez al montar el componente
 
-  return (
-    <div className="mouse-tracker" style={{ left: position.x, top: position.y, color: "white"}}>
-      Posición actual: {position.x}, {position.y}
-    </div>
-  );
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/images.jsx
@@ -0,0 +1,54 @@
+export const images = [
+  {
+    id: 1,
+    imageUrl: 'https://i.pinimg.com/564x/85/7a/85/857a8576946123619580305494d06ed8.jpg',
+    coverUrl: 'src/assets/frame1.png'
+  },
+  {
+    id: 2,
+    imageUrl: 'https://i.pinimg.com/564x/42/fd/11/42fd11278e55128cde3ef20ed14c0510.jpg',
+    coverUrl: 'src/assets/frame2.png'
+  },
+  {
+    id: 3,
+    imageUrl: 'https://i.pinimg.com/564x/a0/3c/92/a03c92bd8800bc1c0e2e5859d8ba36c8.jpg',
+    coverUrl: 'src/assets/frame3.png'
+  },
+  {
+    id: 4,
+    imageUrl: 'https://i.pinimg.com/564x/e6/8d/20/e68d20660743baf3a0e76b6982bacd79.jpg',
+    coverUrl: 'src/assets/frame4.png'
+  },
+
+  {
+    id: 5,
+    imageUrl: 'https://i.pinimg.com/564x/f2/1c/73/f21c73fe5c79071d887ee71d682b54a0.jpg',
+    coverUrl: 'src/assets/frame5.png'
+  },
+  {
+    id: 6,
+    imageUrl: 'https://i.pinimg.com/564x/85/7a/85/857a8576946123619580305494d06ed8.jpg',
+    coverUrl: 'src/assets/frame1.png'
+  },
+  {
+    id: 7,
+    imageUrl: 'https://i.pinimg.com/564x/42/fd/11/42fd11278e55128cde3ef20ed14c0510.jpg',
+    coverUrl: 'src/assets/frame2.png'
+  },
+  {
+    id: 8,
+    imageUrl: 'https://i.pinimg.com/564x/a0/3c/92/a03c92bd8800bc1c0e2e5859d8ba36c8.jpg',
+    coverUrl: 'src/assets/frame3.png'
+  },
+  {
+    id: 9,
+    imageUrl: 'https://i.pinimg.com/564x/e6/8d/20/e68d20660743baf3a0e76b6982bacd79.jpg',
+    coverUrl: 'src/assets/frame4.png'
+  },
+
+  {
+    id: 10,
+    imageUrl: 'https://i.pinimg.com/564x/f2/1c/73/f21c73fe5c79071d887ee71d682b54a0.jpg',
+    coverUrl: 'src/assets/frame5.png'
+  },
+];
