d590819b185304fe96a50a75aefd616c6ca0bddb
TylorDev <jhontmg26@gmail.com>
2024-03-19 16:43:17 -0300

    deploy

--- src/images.js
+++ src/images.js
@@ -2,28 +2,28 @@ export const images = [
   {
     id: 1,
     imageUrl:
-      "https://i.pinimg.com/564x/85/7a/85/857a8576946123619580305494d06ed8.jpg",
+      "https://i.pinimg.com/564x/d2/e6/19/d2e6194a5d35e0e51c10dbc0c0f14ea7.jpg",
     coverUrl: "src/assets/frame2.png",
   },
   {
     id: 2,
 
     imageUrl:
-      "https://i.pinimg.com/564x/42/fd/11/42fd11278e55128cde3ef20ed14c0510.jpg",
+      "https://i.pinimg.com/564x/29/be/f5/29bef5ab15d79dce72b745c5f6a10d95.jpg",
     coverUrl: "src/assets/frame2.png",
   },
   {
     id: 3,
 
     imageUrl:
-      "https://i.pinimg.com/564x/a0/3c/92/a03c92bd8800bc1c0e2e5859d8ba36c8.jpg",
+      "https://i.pinimg.com/564x/82/1e/40/821e403ce4e05cbacc8ff4a987eba69f.jpg",
     coverUrl: "src/assets/frame2.png",
   },
   {
     id: 4,
 
     imageUrl:
-      "https://i.pinimg.com/564x/e6/8d/20/e68d20660743baf3a0e76b6982bacd79.jpg",
+      "https://i.pinimg.com/564x/b2/51/5d/b2515d1dca20da8a568d2e7e0ff0b83c.jpg",
     coverUrl: "src/assets/frame2.png",
   },
 
@@ -31,35 +31,35 @@ export const images = [
     id: 5,
 
     imageUrl:
-      "https://i.pinimg.com/564x/f2/1c/73/f21c73fe5c79071d887ee71d682b54a0.jpg",
+      "https://i.pinimg.com/564x/f2/22/55/f22255f6ba61c4823dfcf19d361c839a.jpg",
     coverUrl: "src/assets/frame2.png",
   },
   {
     id: 6,
 
     imageUrl:
-      "https://i.pinimg.com/564x/85/7a/85/857a8576946123619580305494d06ed8.jpg",
+      "https://i.pinimg.com/564x/6c/48/35/6c4835124d0dfd620051f6de8b1286a2.jpg",
     coverUrl: "src/assets/frame2.png",
   },
   {
     id: 7,
 
     imageUrl:
-      "https://i.pinimg.com/564x/42/fd/11/42fd11278e55128cde3ef20ed14c0510.jpg",
+      "https://i.pinimg.com/564x/d0/bc/fa/d0bcfa4ba5cfb62ab7eaffaf32ff1e7f.jpg",
     coverUrl: "src/assets/frame2.png",
   },
   {
     id: 8,
 
     imageUrl:
-      "https://i.pinimg.com/564x/a0/3c/92/a03c92bd8800bc1c0e2e5859d8ba36c8.jpg",
+      "https://i.pinimg.com/736x/1a/df/11/1adf119713b3a10e1389c185fd983139.jpg",
     coverUrl: "src/assets/frame2.png",
   },
   {
     id: 9,
 
     imageUrl:
-      "https://i.pinimg.com/564x/e6/8d/20/e68d20660743baf3a0e76b6982bacd79.jpg",
+      "https://i.pinimg.com/564x/3a/0d/06/3a0d06e74c7e6aa9e7fa0591a9387359.jpg",
     coverUrl: "src/assets/frame2.png",
   },
 
@@ -67,7 +67,7 @@ export const images = [
     id: 10,
 
     imageUrl:
-      "https://i.pinimg.com/564x/f2/1c/73/f21c73fe5c79071d887ee71d682b54a0.jpg",
+      "https://i.pinimg.com/564x/27/14/4b/27144b7bf47c6100bb63609284bdcbb1.jpg",
     coverUrl: "src/assets/frame2.png",
   },
 ];
